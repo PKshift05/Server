3b5b701cacb61232ddf21a1b27567276a6527504
PKshift05 <quang05012003@gmail.com>
2024-02-19 00:44:29 +0700

    quang

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -77,6 +77,7 @@ export const searchTour = async(req, res)=>{
    
     try {
         let { city, distance, maxGroupSize } = req.params;
+        console.log(city,distance,maxGroupSize);
 
         // Tạo một đối tượng query rỗng
         const query = {};
@@ -88,22 +89,15 @@ export const searchTour = async(req, res)=>{
             city = ""
             query.city = { $regex: city, $options: 'i' };
         }
-
+        
         // Nếu distance được truyền vào, thêm điều kiện tìm kiếm cho distance
         if (distance!=0) {
-            query.distance = { $lte: distance };
-        }else{
-            distance=10000
             query.distance = { $lte: distance };
         }
 
         // Nếu maxGroupSize được truyền vào, thêm điều kiện tìm kiếm cho maxGroupSize
         if (maxGroupSize!=0) {
             query.maxGroupSize = {$lte: maxGroupSize};
-        }else{
-            maxGroupSize=100
-            query.maxGroupSize = {$lte: maxGroupSize};
-
         }
 
         // Thực hiện truy vấn với các điều kiện tìm kiếm đã xây dựng
