01cd1ade623929737c7f25e9d9c675773e3dc428
PKshift05 <quang05012003@gmail.com>
2024-02-22 00:16:43 +0700

    q

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,74 @@
 import User from "../models/User.js";
 
 export const getUsers = async (req, res) => { }
-export const getAllUsers = async (req, res) => { }
-export const login = async (req, res) => { }
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: err });
+    }
+ }
+export const login = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+
+        if (user && req.body.password === user.password) {
+            res.status(200).json({ success: true, message: 'Successfully login', data: user });
+        } else {
+            res.status(401).json({ success: false, message: 'Invalid email or password' });
+        }
+    } catch (err) {
+        console.log("Error in Login", err);
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+}
+
 export const register = async (req, res) => {
     const newUser = new User({
         userName: req.body.userName,
         email: req.body.email,
         password: req.body.password
-      });
-    
-    try{
+    });
+
+    try {
         const savedUser = await newUser.save()
 
-        res.status(200).json({success:true, message:'Successfully created'})
-    }catch(err){
-        res.status(500).json({success:false, message:'Failed to create', 'error': err})
+        res.status(200).json({ success: true, message: 'Successfully created' })
+    } catch (err) {
+        res.status(500).json({ success: false, message: 'Failed to create', 'error': err })
+    }
+}
+export const deleteUser = async(req, res)=>{
+    const id = req.params.id
+    try {
+        
+        await User.findByIdAndDelete(id)
+
+        res.status(200).json({success:true, message:'Successfully deleted'})
+
+    } catch (err) {
+        res.status(500).json({success:false, message:'Failed to delete'})
+        
     }
 }
 
+
+export const updateUser = async(req, res)=>{
+    const id = req.params.id
+    try {
+        
+        const updateUser = await User.findByIdAndUpdate(id,{
+            $set: req.body
+        }, {new:true})
+
+        res.status(200).json({success:true, message:'Successfully updated', data: updateUser})
+
+    } catch (err) {
+        res.status(500).json({success:false, message:'Failed to update'})
+        
+    }
+}
 
--- routers/users.js
+++ routers/users.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { getAllUsers, login, register } from '../controllers/userController.js';
+import { deleteUser, getAllUsers, login, register, updateUser } from '../controllers/userController.js';
 const userRouter =express.Router();
 
 userRouter.get('/', getAllUsers)
 userRouter.post('/login', login)
 userRouter.post('/register', register)
+userRouter.put('/update/:id', updateUser)
+userRouter.delete('/delete/:id', deleteUser)
+
 
 export default userRouter;
