412636613248ab2e2ad7b3dd7676c4204200b6a0
PKshift05 <quang05012003@gmail.com>
2024-02-21 21:50:40 +0700

    q

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ export const getAllUsers = async (req, res) => { }
 export const login = async (req, res) => { }
 export const register = async (req, res) => {
     const newUser = new User({
-        username: req.body.userName || 'defaultUsername',
+        userName: req.body.userName,
         email: req.body.email,
         password: req.body.password
       });
